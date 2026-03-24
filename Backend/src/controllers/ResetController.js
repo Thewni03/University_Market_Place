c0cc109f3419c5c06ef32a6e71bd2ca12aa584d6
Thewni <thewni2003@gmail.com>
2026-03-24 08:49:51 +0530

    password reset part and user authentication part

--- Backend/src/controllers/ResetController.js
+++ Backend/src/controllers/ResetController.js
@@ -23,7 +23,7 @@ export const forgotPassword = async (req, res) => {
             service: "gmail",
             auth: {
                 user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
+                pass: process.env.EMAIL_PASS_OTP
             }
         });
 
